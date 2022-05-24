d700565ebcc8b4bc48fa93e424e5a1ec3e22a6d3
Scratchy <32627001+Scratchdragon@users.noreply.github.com>
2022-05-24 22:21:25 +0000

    Added !auto_del to print auto delete channels

--- main.py
+++ main.py
@@ -210,6 +210,9 @@ async def on_message(message):
 				return
 
 		# Bot admin commands
+		if message.content.startswith('!auto_del') and bot_mod:
+			for key,value in auto_del:
+				await message.channel.send("'" + key.name + "' in guild '" key.guild + "'")
 		if message.content.startswith('!send ') and bot_mod:
 				commands = message.content.split(' ')
 				for guild in client.guilds:
